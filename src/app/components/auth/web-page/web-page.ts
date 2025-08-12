bc011fd8afc8a604f20c3030a60a0ad1ee0d5671
Sergioeling <20207487@gmail.com>
2025-08-12 15:23:33 -0600

    web-page

--- src/app/components/auth/web-page/web-page.ts
+++ src/app/components/auth/web-page/web-page.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthServices } from '../../../core/services/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Login } from '../login/login';
 
 @Component({
   selector: 'app-web-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './web-page.html',
   styleUrl: './web-page.scss'
 })
-export class WebPage {
+export class WebPage {  
+  private auth = inject(AuthServices);
+  private router = inject(Router);
+
+   constructor(public modalService: NgbModal) {}
+
+  
+
+  openAddModal(item?: any, edit?: boolean): void {
+    const modalRef = this.modalService.open(Login, {
+      backdrop: 'static',
+      size: 'lg',
+    });
+  }
+ 
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  }
 
 }
