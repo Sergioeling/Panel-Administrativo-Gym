f94bc2eff1d239eb5bbebeee83fbc4798f37d932
Sergioeling <20207487@gmail.com>
2025-08-18 10:15:03 -0600

    a

--- src/app/app-settings.service.ts
+++ src/app/app-settings.service.ts
@@ -12,11 +12,6 @@ export class AppSettingsService {
 
   public static getHeaders(includeToken: boolean = false): Record<string, string> {
     const headers: Record<string, string> = {
-      //'Access-Control-Allow-Origin': '*',
-      //'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
-      //'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept,Authorization',
-      //'Access-Control-Allow-Credentials': 'true',
-      //'Access-Control-Max-Age': '3600',
       'Content-Type': 'application/json'
     };
 
--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -77,6 +77,7 @@ export class HttpServices {
   }
 
 
+  //services
   obtenerDietas(): Observable<any> {
     return this.get('obtener-dietas');
   }
