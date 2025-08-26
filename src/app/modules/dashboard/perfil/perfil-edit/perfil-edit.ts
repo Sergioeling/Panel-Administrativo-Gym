b0efe7d3936a332543364f42f12bae75f5c3485b
MindflyStudioAR <148261937+MindflyStudioAR@users.noreply.github.com>
2025-08-26 12:58:42 -0600

    perfil

---
+++ src/app/modules/dashboard/perfil/perfil-edit/perfil-edit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-edit',
+  imports: [],
+  templateUrl: './perfil-edit.html',
+  styleUrl: './perfil-edit.scss'
+})
+export class PerfilEdit {
+
+}
--- src/app/modules/dashboard/perfil/perfil.ts
+++ src/app/modules/dashboard/perfil/perfil.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+//import { PerfilEditModal } from './perfil-edit.modal'; // 👉 lo vamos a crear
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './perfil.html',
-  styleUrl: './perfil.scss'
+  styleUrls: ['./perfil.scss']
 })
 export class Perfil {
+  private modalService = inject(NgbModal);
 
+  user = {
+    nombre: 'Admin PowerGym',
+    email: 'admin@powergym.com',
+    rol: 'ADMIN',
+    ultimoAcceso: 'Hoy - 14:30'
+  };
+/*
+  editarPerfil() {
+    const modalRef = this.modalService.open(PerfilEditModal, { centered: true });
+    modalRef.componentInstance.userData = { ...this.user };
+
+    modalRef.result.then((result) => {
+      if (result) {
+        this.user = result; // Actualizar datos en el perfil
+      }
+    }).catch(() => { });
+  }
+    */
 }
