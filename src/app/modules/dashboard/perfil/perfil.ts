ae5d24ccfab229f704c2bd8eecf2edb37540a0ca
MindflyStudioAR <148261937+MindflyStudioAR@users.noreply.github.com>
2025-09-02 10:08:56 -0600

    perfilResponsivo, consumo de servicios de perfil

--- src/app/modules/dashboard/perfil/perfil.ts
+++ src/app/modules/dashboard/perfil/perfil.ts
@@ -1,31 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthServices } from '../../../core/services/auth/auth.service';//---1
 import { HttpServices } from '../../../core/services/http/http.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './perfil.html',
-  styleUrls: ['./perfil.scss']  
+  styleUrls: ['./perfil.scss']
 })
 export class Perfil {
-  editMode = false; 
-  cambiarPassMode = false; 
+  editMode = false;
+  cambiarPassMode = false;
   perfilForm: FormGroup;
   passwordForm: FormGroup;
-  private originalData: any; 
+  private originalData: any;
 
-  private authService = inject(AuthServices)//---------2
   protected httpService = inject(HttpServices);
 
   constructor(private fb: FormBuilder) {
     this.perfilForm = this.fb.group({
-      nombre: ['Admin PowerGym', Validators.required],
-      email: ['admin@powergym.com', [Validators.required, Validators.email]],
-      ultimoAcceso: ['Hoy - 14:30']
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      fecha_de_registro: ['']
     });
 
     this.passwordForm = this.fb.group({
@@ -36,22 +35,29 @@ export class Perfil {
 
   }
 
-   ngOnInit(): void {
+  ngOnInit(): void {
     this.obtenerusuario();
   }
 
-  obtenerusuario(){
+  obtenerusuario() {
     this.httpService.getUsuarios().subscribe({
-      next: (data: any) => {
-        console.log('Datos del usuario:', data);
+      next: (response: any) => {
+        //console.log('Datos del usuario:', response);
+        const user = response.data; 
+        this.originalData = user; 
 
-        this.perfilForm.patchValue(data);
+        this.perfilForm.patchValue({
+          nombre: user.nombre,
+          email: user.correo,
+          fecha_de_registro: user.fecha_registro
+        });
       },
       error: (error) => {
         console.error('Error al obtener usuario:', error);
       }
     });
   }
+
 
   toggleEdit() {
     if (!this.editMode) {
@@ -64,10 +70,40 @@ export class Perfil {
 
   guardarCambios() {
     if (this.perfilForm.valid) {
-      console.log('Datos guardados:', this.perfilForm.value);
       this.editMode = false;
+      const datos = this.perfilForm.value;
+
+      this.httpService.updateUser({
+        id: this.originalData.id,   
+        nombre: datos.nombre,
+        correo: datos.email
+      }).subscribe({
+        next: (res: any) => {
+          //console.log('Usuario actualizado correctamente:', res);
+          Swal.fire({
+            title: 'Usuario Actualizado',
+            text: 'Usuario actualizado correctamente',
+            icon: 'success',
+            timer: 3000,
+            showConfirmButton: false,
+            timerProgressBar: true
+          });
+        },
+        error: (err) => {
+          //console.error('Error al actualizar usuario:', err);
+           Swal.fire({
+            title: 'Error',
+            text: err?.error?.message || 'Error al actualizar usuario:',
+            icon: 'error',
+            timer: 3000,
+            showConfirmButton: false,
+            timerProgressBar: true
+          });
+        }
+      });
     }
   }
+
 
   toggleCambiarPass() {
     this.cambiarPassMode = !this.cambiarPassMode;
@@ -75,14 +111,51 @@ export class Perfil {
 
   guardarPassword() {
     if (this.passwordForm.valid) {
-      const { nueva, confirmar } = this.passwordForm.value;
+      const { actual, nueva, confirmar } = this.passwordForm.value;
+
       if (nueva !== confirmar) {
-        alert('Las contraseñas no coinciden');
+        Swal.fire({
+          title: 'Error',
+          text: 'Las contraseñas no coinciden',
+          icon: 'error',
+          timer: 3000,
+          showConfirmButton: false,
+          timerProgressBar: true
+        });
         return;
       }
-      console.log('Contraseña cambiada a:', nueva);
-      this.cambiarPassMode = false;
-      this.passwordForm.reset();
+
+      this.httpService.updatePassword({
+        actual: actual,
+        nueva: nueva
+      }).subscribe({
+        next: (res: any) => {
+          //console.log('Contraseña actualizada correctamente:', res);
+          Swal.fire({
+            title: 'Contraseña Actualizada',
+            text: 'Tu contraseña se cambió exitosamente',
+            icon: 'success',
+            timer: 3000,
+            showConfirmButton: false,
+            timerProgressBar: true
+          });
+
+          this.cambiarPassMode = false;
+          this.passwordForm.reset();
+        },
+        error: (err) => {
+          //console.error('Error al cambiar contraseña:', err);
+          Swal.fire({
+            title: 'Error',
+            text: err?.error?.message || 'No se pudo cambiar la contraseña',
+            icon: 'error',
+            timer: 3000,
+            showConfirmButton: false,
+            timerProgressBar: true
+          });
+        }
+      });
     }
   }
+
 }
