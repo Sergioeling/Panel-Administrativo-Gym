55ca10290bba95b4dbeaf8f28960879739f4d62f
ReyesJorgeLuis <145588183+ReyesJorgeLuis@users.noreply.github.com>
2026-01-23 11:57:41 -0600

    Avance de tabla platillos

--- src/app/modules/dashboard/revision/revision.ts
+++ src/app/modules/dashboard/revision/revision.ts
@@ -8,6 +8,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RevisionService } from './revision.service';
 import { AltaPlatillos } from '../../shared/modales/alta-platillos/alta-platillos';
 import { VistaDocumentos } from '../../shared/modales/vista-documentos/vista-documentos';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -164,8 +165,54 @@ cargarPlatillosPendientes(): void {
 
 
   rechazar(item: any): void {
-    console.log('Rechazar', item);
-  }
+  Swal.fire({
+    title: 'Rechazar platillo',
+    text: 'Escribe el motivo del rechazo',
+    input: 'textarea',
+    inputPlaceholder: 'Motivo del rechazo...',
+    inputAttributes: {
+      'aria-label': 'Motivo del rechazo'
+    },
+    showCancelButton: true,
+    confirmButtonText: 'Rechazar',
+    cancelButtonText: 'Cancelar',
+    confirmButtonColor: '#dc2626',
+    inputValidator: (value) => {
+      if (!value || !value.trim()) {
+        return 'El motivo es obligatorio';
+      }
+      return null;
+    }
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const motivo = result.value;
+
+      this.revisionService
+        .revisionPlatillo(item.id, 0, motivo)
+        .subscribe({
+          next: () => {
+            Swal.fire({
+              icon: 'success',
+              title: 'Platillo rechazado',
+              text: 'Se notificó al nutricionista',
+              timer: 2500,
+              showConfirmButton: false
+            });
+
+            this.cargarPlatillosPendientes();
+          },
+          error: () => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'No se pudo rechazar el platillo'
+            });
+          }
+        });
+    }
+  });
+}
+
 
   verArchivos(item: any): void {
       const modalRef = this.modalService.open(
