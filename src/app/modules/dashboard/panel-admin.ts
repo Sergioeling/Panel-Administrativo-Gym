8bab74f0e32acc221faacd1065d9156a60497e8f
ReyesJorgeLuis <145588183+ReyesJorgeLuis@users.noreply.github.com>
2026-01-28 13:43:43 -0600

    Avance de tabla platillos

--- src/app/modules/dashboard/panel-admin.ts
+++ src/app/modules/dashboard/panel-admin.ts
@@ -153,7 +153,7 @@ export class PanelAdmin implements OnInit {
       },
       {
         title: 'Gestión de Alimentos', icon: null, bi: 'bi-egg-fried', id: 'dietas', role: 'ADMIN,NUTRICIONISTA,USUARIO', options: [
-          { title: 'Alimentos', route: 'alimentos', role: 'ADMIN', icon: null, bi: 'bi-egg-fried' },
+          { title: 'Alimentos', route: 'alimentos', role: '', icon: null, bi: 'bi-egg-fried' },
           { title: 'Platillos', route: 'platillos-nutricionista', role: 'NUTRICIONISTA', icon: null, bi: 'bi-cup-hot' },
         ]
       },
